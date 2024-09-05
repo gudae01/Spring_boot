83bd4227e331d26114775d3fcef9e30ae31bb948
gudae01 <guseorn@gmail.com>
2024-09-05 17:28:25 +0900

    RestAPI-GET

---
+++ RestApi-GET/src/test/java/com/example/restapiget/RestApiGetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapiget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiGetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
